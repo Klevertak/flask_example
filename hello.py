5a40c126f8cb2b7187b24fef61fdfe6d7f333903
klevertak <klevertak@mail.ru>
2017-05-20 14:49:14 +0300

    get screenshot
    get screenshot

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 """Cloud Foundry test"""
 from flask import Flask
+from selenium import webdriver
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver import ActionChains
 import os
 
 app = Flask(__name__)
@@ -8,6 +11,14 @@ port = int(os.getenv("PORT"))
 
 @app.route('/')
 def hello_world():
+    # REPLACE WITH YOUR DRIVER PATH. EXAMPLES FOR CHROME AND PHANTOMJS
+    driver = webdriver.PhantomJS(executable_path='../phantomjs-2.1.1-macosx/bin/phantomjs')
+    # driver = webdriver.Chrome(executable_path='../chromedriver/chromedriver')
+
+    driver.implicitly_wait(5)
+    driver.get('http://www.pythonscraping.com/')
+    driver.get_screenshot_as_file('tmp/pythonscraping.png')
+
     return 'Hello World! I am running on port ' + str(port)
 
 if __name__ == '__main__':
--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,22 @@
-from urllib.request import urlopen
+from urllib.request import urlopen,Request
 from bs4 import BeautifulSoup
 import re
+from  urllib.parse import urlparse, urlencode
+
+
+gv_search_sites = [
+   "https://www.indeed.com",
+   "http://www.simplyhired.com",
+   "https://jobsearch.gov.au",
+   "http://www.careerbuilder.com",
+   "http://www.headhunter.com"
+]
+
+gv_search_url = 'https://www.google.ru/#newwindow=1&q=site:monster.com+C/C%2B%2B'
+
+
+def splitAddress(address):
+    return urlparse(address).netloc.replace("www.","")
 
 # https://github.com/REMitchell/python-scraping
 #Retrieves a list of all Internal links found on a page
@@ -14,18 +30,37 @@ def getInternalLinks(bsObj, includeUrl):
     return internalLinks
 
 
+def initial_search(keyword, site_url):
+    # html = urlopen(gv_search_url+site_url+'+'+keyword)
+    values = {'q': 'C/C++'}
+    data = urlencode(values)
+    data = data.encode('utf-8')  # data should be bytes
+    req = Request(gv_search_url, data)
+    resp = urlopen(gv_search_url)
+
+    print(resp.read())
+    return
+
+
+
 def main():
     # html = urlopen("https://www.google.com/")
     # html = urlopen("https://www.indeed.com/")
-    # html = urlopen("https://www.monster.com/")site:monster.com abap
-    html = urlopen('https://e-commerce.severstal.com/')
+    # html = urlopen("https://www.monster.com/") #site:monster.com abap
+    # html = urlopen('https://e-commerce.severstal.com/')
+    # html = urlopen("http://www.headhunter.com/")
 
-    bsObj = BeautifulSoup(html.read(),'html.parser')
+    # for s in gv_search_sites:
+    #     print(splitAddress(s))
+    #
+    # initial_search('C/C++',gv_search_sites[0])
 
-    internalLinks = getInternalLinks(bsObj,'')
-
-    for l in internalLinks:
-        print(l)
+    # bsObj = BeautifulSoup(html.read(),'html.parser')
+    #
+    # internalLinks = getInternalLinks(bsObj,'')
+    #
+    # for l in internalLinks:
+    #     print(l)
     # for link in bsObj.find_all('input'):
     #     print(link.attrs)
     return
